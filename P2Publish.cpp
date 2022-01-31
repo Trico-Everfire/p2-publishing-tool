89eb56fe88b2747d8dda60fa2d17707ca8652822
Trico-Everfire <bobbys@live.nl>
2022-01-31 05:57:37 +0100

    Fixed styling issues causing problems.

--- P2Publish.cpp
+++ P2Publish.cpp
@@ -4,27 +4,46 @@
 #include <QApplication>
 #include <QIcon>
 #include <QSharedMemory>
+#include <QCommonStyle>
 #include <QStyleFactory>
 
 int main( int argc, char **argv )
 {
 	QApplication app( argc, argv );
 	app.setWindowIcon( QIcon( ":/zoo_textures/P2 Publisher Icon2.png" ) );
-	// QFile file(":/dark/stylesheet.qss");
-	// file.open(QFile::ReadOnly | QFile::Text);
-	// QTextStream stream(&file);
-	// app.setStyleSheet(stream.readAll());
-	qInfo() << app.setStyle("Fusion");
+	QCommonStyle* style = (QCommonStyle*) QStyleFactory::create("fusion");
+	app.setStyle(style);
 
-	bool performSingleCheck = true;
-	for(int i = 0; i < argc; i++){
-		qInfo() << (QString(argv[i]));
-		if(QString(argv[i]) == "-overrideSingleOnly"){
-			performSingleCheck = false;
-		}
-	}
+	QPalette palette = QPalette();
+	palette.setColor(QPalette::Window, QColor(49,54,59));
+	palette.setColor(QPalette::WindowText, Qt::white);
+	palette.setColor(QPalette::Base, QColor(27,30,32));
+	palette.setColor(QPalette::AlternateBase, QColor(49,54,59));
+	palette.setColor(QPalette::ToolTipBase, Qt::black);
+	palette.setColor(QPalette::ToolTipText, Qt::white);
+	palette.setColor(QPalette::Text, Qt::white);
+	palette.setColor(QPalette::Button, QColor(49,54,59));
+	palette.setColor(QPalette::ButtonText, Qt::white);;
+	palette.setColor(QPalette::BrightText, Qt::red);
+	palette.setColor(QPalette::Link, QColor(42, 130, 218));
+	palette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+	palette.setColor(QPalette::HighlightedText, Qt::black);
+	palette.setColor(QPalette::Active, QPalette::Button, QColor(49,54,59));
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
+    palette.setColor(QPalette::Disabled, QPalette::WindowText, Qt::darkGray);
+    palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
+    palette.setColor(QPalette::Disabled, QPalette::Light, QColor(49,54,59));
+	app.setPalette(palette);
+
+	// bool performSingleCheck = true;
+	// for(int i = 0; i < argc; i++){
+	// 	qInfo() << (QString(argv[i]));
+	// 	if(QString(argv[i]) == "-overrideSingleOnly"){
+	// 		performSingleCheck = false;
+	// 	}
+	// }
 	
-	qInfo() << performSingleCheck;
+	// qInfo() << performSingleCheck;
 	
 		
 	// QSharedMemory shared( "qt_Portal_2_Map_Publisher" );
