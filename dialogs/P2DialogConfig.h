9b2dd0fff1722a92bb2430024fd7b53f6886688d
Trico-Everfire <bobbys@live.nl>
2023-01-26 20:19:00 +0100

    CLEANUP

--- dialogs/P2DialogConfig.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QFileDialog>
-
-#if _WIN32
-static constexpr const auto FILE_PICKER_OPTS = static_cast<QFileDialog::Option>( 0 );
-#else
-static constexpr const auto FILE_PICKER_OPTS = QFileDialog::Option::DontUseNativeDialog;
-#endif
